f840e76970008832fbd985349c8ff13a0e243b7a
HarshitRai121 <129781509+HarshitRai121@users.noreply.github.com>
2024-07-17 23:59:58 +0530

    Update QuestionsSection.jsx
    previous question bug solved

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -18,7 +18,9 @@ function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
     <div  className='p-5 border rounded-lg my-10'>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
       {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
-        <h2 className={`p-2 bg-secondary rounded-full
+        <h2 
+        key={index}
+        className={`p-2 bg-secondary rounded-full
         text-xs md:text-sm text-center cursor-pointer
         ${activeQuestionIndex===index&&'font-bold text-blue-500 bg-primary'} `}>Question #{index+1}</h2>
       ))}
